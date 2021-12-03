eef3cb3bddd881d1dcdcfc769ec8509c6b7e9e50
Trey Morris <treymorrisit@hotmail.com>
2021-12-03 17:49:58 -0500

    working on the functions to make it work

--- src/App.js
+++ src/App.js
@@ -4,23 +4,65 @@ import Wrapper from "./components/Wrapper";
 import Navbar from "./components/Navbar";
 import Display from "./components/Display";
 import Cards from "./components/Cards"
-
+import cardInfo from "./components/cardInfo.json"
 
 function App() {
 
-  const [card, setCards] = useState('');
+  const [cards, setCards] = useState({
+    clicked: false });
+
   const [score, setScore] = useState({
     highScore: 0,
     currentScore: 0,
   });
 
-  const [clicked, setClicked] = useState(false);
+  const handleClick = id => {
+    shuffleCards();
+    updateScore(id);
+  }
+
+  const updateScore = id => {
+
+  }
+
+  const shuffleCards = () => {
+    
+    
+  };
+
   
+  const shuffle = array => {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    
+    while (0 !== currentIndex) {
+      
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+
+    }
+
+    return array;
+    
+  };
+
   return (
     <Wrapper>
       <Navbar/>
       <Display/>
-      <Cards/>
+      <Cards onClick={handleClick}
+             Clicked={cards.clicked}
+             id={cardInfo.id}
+             key={cardInfo.id}
+             name={cardInfo.name}
+             image={cardInfo.image}
+             occupation={cardInfo.occupation}
+             />
     </Wrapper>
   );
 }
